916e80678d69c82f6e8954631ef0e77c568829b0
AsLaNQaRiBoF <126081468+AsLaNQaRiBoF@users.noreply.github.com>
2023-04-25 13:31:57 +0400

    Add files via upload

---
+++ Новая папка (2)/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Runtime.Intrinsics.Arm;
+
+namespace ConsoleApp1
+{
+    internal class Program:Bookparams
+    {
+        static void Main(string[] args)
+        {
+        
+        }
+        public void Remove()
+        {
+            using (BookContext db = new BookContext())
+            {
+                db.Database.BookName = Console.WriteLine;
+                db.Stock.Where(d => d.Name == "Accounts").First();
+                db.Stock.Remove(db.Stock.Where(d => d.Name == "Accounts").First(););
+                db.SaveChanges();
+
+                Console.WriteLine("Book {0} ({1}) is Deleted " );
+                Console.ReadKey();
+            }
+        }
+    }
+}
